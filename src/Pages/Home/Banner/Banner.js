63b4c63cf0322ee511074e9deebff98bc2c87fd3
unknown <rahataminulislam37@gmail.com>
2022-11-24 13:17:50 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="hero w-2/3 my-10	 mx-auto ">
+            <div className="hero-content flex-col lg:flex-row">
+                <img alt='' src="https://storage.googleapis.com/gs-images/gs-blog/2021/06/524c304c-aa.jpg" />
+                <div>
+                    <h1 className="text-5xl font-bold">Welcome To Trust Resale Market !!  </h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
