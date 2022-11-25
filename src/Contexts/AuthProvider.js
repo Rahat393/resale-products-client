144d2651532a51caa3d9e3bb8beac029287716fe
unknown <rahataminulislam37@gmail.com>
2022-11-25 19:30:57 +0600

    firebase added

---
+++ src/Contexts/AuthProvider.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+import { createContext } from "react";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user observer');
+            setUser(currentUser)
+            setLoading(false)
+        });
+        return () => unsubscribe()
+    }, [])
+
+    const authInfo = {
+        user,
+        loading
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
